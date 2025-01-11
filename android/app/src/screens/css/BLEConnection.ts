a33bd447024d2cdcb95621a46d4fca8b008ea112
OHHYUKJUN <kmh7277@naver.com>
2025-01-11 17:45:52 +0900

    Design: style코드 분리

---
+++ android/app/src/screens/css/BLEConnection.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  text: {
+    fontSize: 16,
+    color: '#333333',
+    marginBottom: 20,
+  },
+});
+
+export default styles;
