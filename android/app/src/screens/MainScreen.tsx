1e3d082c04b9c0f6d922bcd67d2fb359713a84b1
OHHYUKJUN <kmh7277@naver.com>
2025-01-13 09:30:08 +0900

    Feat: react-native navigation 적용

--- android/app/src/screens/MainScreen.tsx
+++ android/app/src/screens/MainScreen.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import styles from './css/MainScreen';
 
-const MainScreen = ({ onNavigateToBluetooth }: { onNavigateToBluetooth: () => void }) => {
+type MainScreenProps = {
+  navigation: any; // React Navigation의 navigation 객체
+};
+
+const MainScreen = ({ navigation }: MainScreenProps) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to the App</Text>
-      <Button title="Go to Bluetooth Connection" onPress={onNavigateToBluetooth} />
+      <Button title="Go to Bluetooth Connection" onPress={() => navigation.navigate('Bluetooth')} />
     </View>
   );
 };
