dcb167234f7ac643339179dab0dc559f14ce148f
OhHyukJun <kmh7277@naver.com>
2025-01-14 21:51:20 +0900

    Feat: 경로 연결

--- App.tsx
+++ App.tsx
@@ -6,8 +6,9 @@ import { RecoilRoot } from 'recoil';
 import SplashScreen from './android/app/src/screens/SplashScreen';
 import MainScreen from './android/app/src/screens/MainScreen';
 import BLEConnection from './android/app/src/screens/BLEConnection';
-import LoginScreen from './android/app/src/screens/auth/LoginScreen';
-import RegisterScreen from './android/app/src/screens/auth/RegisterScreen';
+import LoginScreen from './android/app/src/screens/auth/Login/LoginScreen';
+import RegisterScreen from './android/app/src/screens/auth/Register/RegisterScreen';
+import PersonalInfo from './android/app/src/screens/auth/Register/PersonalInfo';
 // React Query Client 생성
 // const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => {
           <Stack.Screen name="Splash" component={SplashScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Main" component={MainScreen} />
+          <Stack.Screen name="Info" component={PersonalInfo} />
           <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="Bluetooth" component={BLEConnection} />
         </Stack.Navigator>
