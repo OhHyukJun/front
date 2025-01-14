6034d35c426812a76f8ccae0cf245da4eb445385
OhHyukJun <kmh7277@naver.com>
2025-01-14 17:15:25 +0900

    Feat: 회원가입 코드 세팅

---
+++ android/app/src/screens/auth/RegisterScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RegisterScreen = () => {
+  return (
+    <View>
+      <Text>회원가입 화면</Text>
+    </View>
+  );
+};
+
+export default RegisterScreen;
