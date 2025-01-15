cd2636f7077f2e874d47f0c5a5bf2c07cf6d2532
OhHyukJun <kmh7277@naver.com>
2025-01-15 13:47:48 +0900

    Feat: 개인정보 화면 구현 과정

--- android/app/src/screens/auth/Register/PersonalInfo.tsx
+++ android/app/src/screens/auth/Register/PersonalInfo.tsx
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, Button, TouchableOpacity, ScrollView } from 'react-native';
 import styles from '../../css/auth/Register/PersonalInfo';
 
 type PersonalInfoProps = {
@@ -22,30 +22,79 @@ const PersonalInfo = ({ navigation }: PersonalInfoProps) => {
         if (isChecked && isCheckedTwo) {
             navigation.replace('Register');
         }
-    }, [isChecked, isCheckedTwo, navigation]);    
+    }, [isChecked, isCheckedTwo, navigation]);
 
     return (
         <View style={styles.container}>
+            <View style={styles.headerContainer}>
+                <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+                    <Text style={styles.backButtonText}>{'<'}</Text>
+                </TouchableOpacity>
+                <Text style={styles.headerTitle}>약관 동의서</Text>
+            </View>
+            <View style={styles.divider}/>
             <Text style={styles.title}>나비잠에 오신 것을 환영합니다.</Text>
             <Text style={styles.subtitle}>약관에 동의 후 이용이 가능합니다.</Text>
-            <TouchableOpacity style={styles.description} onPress={handleCheck}>
+            <TouchableOpacity style={styles.descriptionContainer} onPress={handleCheck}>
                 <Image 
-                    source={isChecked 
-                        ? require('../../img/checkInfo_on.png') 
-                        : require('../../img/checkInfo_off.png')}
+                    source={isChecked
+                        ? require('../../img/checkbox_on.png')
+                        : require('../../img/checkbox_off.png')}
                     style={styles.checkIcon}
                 />
-                <Text>개인정보 이용/제공/활용 동의</Text>
+                <Text style={styles.description}>개인정보 이용/제공/활용 동의</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.description} onPress={handleCheckTwo}>
-                <Image 
-                    source={isCheckedTwo 
-                        ? require('../../img/checkInfo_on.png') 
-                        : require('../../img/checkInfo_off.png')}
+            <View style={styles.infoContainer}>
+                <ScrollView contentContainerStyle={styles.scrollContent}>
+                    <Text style={styles.infoText}>
+                        「정보통신망 이용촉진 및 정보보호에 관한 법률」 및
+                        「개인정보보호법」 등 관련 법령상의 개인정보보호 규정을 준수하여
+                        「나비잠」 사용자의 개인정보 및 권익을 보호하고, 개인정보와 관련한
+                        참가자의 고충을 원활하게 처리할 수 있도록 다음과 같은 개인정보
+                        처리방침을 두고 있습니다.
+                        「정보통신망 이용촉진 및 정보보호에 관한 법률」 및
+                        「개인정보보호법」 등 관련 법령상의 개인정보보호 규정을 준수하여
+                        「나비잠」 사용자의 개인정보 및 권익을 보호하고, 개인정보와 관련한
+                        참가자의 고충을 원활하게 처리할 수 있도록 다음과 같은 개인정보
+                        처리방침을 두고 있습니다.
+                        「정보통신망 이용촉진 및 정보보호에 관한 법률」 및
+                        「개인정보보호법」 등 관련 법령상의 개인정보보호 규정을 준수하여
+                        「나비잠」 사용자의 개인정보 및 권익을 보호하고, 개인정보와 관련한
+                        참가자의 고충을 원활하게 처리할 수 있도록 다음과 같은 개인정보
+                        처리방침을 두고 있습니다.
+                    </Text>
+                </ScrollView>
+            </View>
+            <TouchableOpacity style={styles.descriptionContainer} onPress={handleCheckTwo}>
+                <Image
+                    source={isCheckedTwo
+                        ? require('../../img/checkbox_on.png')
+                        : require('../../img/checkbox_off.png')}
                     style={styles.checkIcon}
                 />
-                <Text>이용 약관 동의</Text>
+                <Text style={styles.description}>이용 약관 동의</Text>
             </TouchableOpacity>
+            <View style={styles.infoContainer}>
+                <ScrollView contentContainerStyle={styles.scrollContent}>
+                    <Text style={styles.infoText}>
+                        「정보통신망 이용촉진 및 정보보호에 관한 법률」 및
+                        「개인정보보호법」 등 관련 법령상의 개인정보보호 규정을 준수하여
+                        「나비잠」 사용자의 개인정보 및 권익을 보호하고, 개인정보와 관련한
+                        참가자의 고충을 원활하게 처리할 수 있도록 다음과 같은 개인정보
+                        처리방침을 두고 있습니다.
+                        「정보통신망 이용촉진 및 정보보호에 관한 법률」 및
+                        「개인정보보호법」 등 관련 법령상의 개인정보보호 규정을 준수하여
+                        「나비잠」 사용자의 개인정보 및 권익을 보호하고, 개인정보와 관련한
+                        참가자의 고충을 원활하게 처리할 수 있도록 다음과 같은 개인정보
+                        처리방침을 두고 있습니다.
+                         「개인정보보호법」 등 관련 법령상의 개인정보보호 규정을 준수하여
+                        「나비잠」 사용자의 개인정보 및 권익을 보호하고, 개인정보와 관련한
+                        참가자의 고충을 원활하게 처리할 수 있도록 다음과 같은 개인정보
+                        처리방침을 두고 있습니다.
+                    </Text>
+                </ScrollView>
+            </View>
+            <Button title='확인하기'/>
         </View>
     );
 };
--- android/app/src/screens/css/auth/Register/PersonalInfo.ts
+++ android/app/src/screens/css/auth/Register/PersonalInfo.ts
@@ -2,48 +2,79 @@ import { StyleSheet } from 'react-native';
 import { vw, vh } from 'react-native-expo-viewport-units';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FDFCFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    width: '80%',
-    textAlign: 'center',
-    fontWeight: '400',
-    fontSize: 20,
-    lineHeight: 42,
-    color: '#6D73C6',
-    textShadowColor: 'rgba(0, 0, 0, 0.25)',
-    textShadowOffset: { width: 0, height: 4 },
-    textShadowRadius: 4,
-  },
-
-  subtitle: {
-    width: '80%',
-    textAlign: 'center',
-    fontWeight: '400',
-    fontSize: 15,
-    marginBottom: vw(3),
-    color: '#6D73C6',
-  },
-
-  description: {
-    width: '80%',
-    textAlign: 'center',
-    fontWeight: '400',
-    fontSize: 12,
-    color: '#6D73C6',
-    flexDirection: 'row',
-  },
-  checkIcon: {
-    width: vw(5),
-    height: vw(5),
-    marginTop: vw(1.5),
-    marginRight: vw(4),
-  },
+    container: {
+      flex: 1,
+      backgroundColor: '#FDFCFF',
+      alignItems: 'center',
+    },
+    headerContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      height: vh(10),
+      paddingHorizontal: vw(5),
+    },
+    backButton: {
+      position: 'absolute',
+      left: vw(5),
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: vw(8),
+      height: vh(5),
+    },
+    backButtonText: {
+      color: '#6D73C6',
+      fontSize: vw(4.5),
+    },
+    headerTitle: {
+      textAlign: 'center',
+      fontSize: vw(6),
+      color: '#6D73C6',
+      fontWeight: '600',
+    },
+    divider: {
+      width: '85%',
+      borderTopWidth: 1,
+      borderColor: '#4750BD',
+    },
+    title: {
+      width: '80%',
+      textAlign: 'center',
+      fontWeight: '400',
+      fontSize: vw(6),
+      lineHeight: 42,
+      color: '#6D73C6',
+      marginTop: vh(2),
+    },
+    subtitle: {
+      width: '80%',
+      textAlign: 'center',
+      fontWeight: '400',
+      fontSize: vw(3.5),
+      marginBottom: vw(3),
+      color: '#6D73C6',
+    },
+    descriptionContainer: {
+      width: '80%',
+      flexDirection: 'row',
+      alignItems: 'center', // 세로 중앙 정렬
+      justifyContent: 'flex-start', // 왼쪽 정렬
+      height: vw(8), // 고정 높이 설정
+      marginVertical: vw(1), // 위아래 적당한 여백 추가
+    },
+    checkIcon: {
+      width: vw(5),
+      height: vw(5),
+      marginRight: vw(2), // 아이콘과 텍스트 간 간격
+      marginBottom: vw(1),
+    },
+    description: {
+      fontWeight: '400',
+      fontSize: vw(4), // 적당한 반응형 폰트 크기
+      lineHeight: vw(4.5), // 폰트 크기와 동일한 라인 높이
+      marginTop: 0, // 기본 마진 제거
+    },
 
   icon: {
     width: 50,
@@ -61,20 +92,23 @@ const styles = StyleSheet.create({
     borderTopColor: '#6D73C6',
   },
 
-  group: {
+  infoContainer: {
     width: '80%',
+    height: vh(20),
     flexDirection: 'column',
     alignItems: 'center',
     marginVertical: 10,
-  },
-
-  groupLarge: {
-    width: '60%',
-    height: 77,
-    alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#f0f0f0',
     borderRadius: 10,
+  },
+
+  scrollContent: {
+    padding: 10, // 스크롤 내용에 여백 추가
+  },
+
+  infoText:{
+    padding:vw(3),
+    fontSize: vw(3),
   },
 
   responsiveText: {
