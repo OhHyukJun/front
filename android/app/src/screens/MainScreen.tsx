e76a3c186cad9da8409b13fd31870ce550260dc4
OhHyukJun <kmh7277@naver.com>
2025-01-22 09:30:55 +0900

    Feat: 블루투스 연결 기능 구현

--- android/app/src/screens/MainScreen.tsx
+++ android/app/src/screens/MainScreen.tsx
@@ -8,11 +8,34 @@ type MainScreenProps = {
 };
 
 const MainScreen = ({ navigation }: MainScreenProps) => {
-  const { connectToDevice, connectedDevice } = useBluetooth();
+  // const { findDeviceAndSendData, disconnectDevice } = useBluetooth();
+  const { connectToDevice } = useBluetooth();
 
-  const handleBluetooth = () => {
-    connectToDevice();
+  /* const handleBluetooth = async () => {
+    try {
+      await findDeviceAndSendData('r'); // Attempt to find, connect, and send data
+    } catch (error) {
+      console.error('Error during Bluetooth handling:', error.message || error);
+    }
   };
+
+    const handleDisconnect = async () => {
+    try {
+      await disconnectDevice(); // Bluetooth 연결 해제
+    } catch (error) {
+      console.error('Error during Bluetooth disconnection:', error.message || error);
+    }
+  };
+  */
+  const handleBluetooth = async () => {
+    try {
+      await connectToDevice(); // Attempt to find, connect, and send data
+    } catch (error: any) {
+      console.error('Error during Bluetooth handling:', error.message || error);
+    }
+  };
+
+
   return (
     <View style={styles.container}>
       <View style={styles.roundedContainer}>
@@ -21,7 +44,9 @@ const MainScreen = ({ navigation }: MainScreenProps) => {
             아이를 클릭해{'\n'}녹음해주세요 :)
           </Text>
         </ImageBackground>
-        <Image source={require('./img/baby_profile.jpg')} style={styles.baby} />
+        <TouchableOpacity onPress={handleBluetooth}>
+          <Image source={require('./img/baby_profile.jpg')} style={styles.baby} />
+        </TouchableOpacity>
         {/* chatbot 이미지에 TouchableOpacity 추가 */}
         <TouchableOpacity
           style={styles.chatbot}
