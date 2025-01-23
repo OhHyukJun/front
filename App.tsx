3e0e492bd51ef7ab3d8deffb65b7aa10d03357e7
OhHyukJun <kmh7277@naver.com>
2025-01-23 15:51:44 +0900

    Feat: 경로 변경

--- App.tsx
+++ App.tsx
@@ -4,9 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import { QueryClient } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import SplashScreen from './android/app/src/screens/SplashScreen';
-import HomeScreen from './android/app/src/screens/HomeScreen';
-import ChatbotScreen from './android/app/src/screens/ChatbotScreen';
-import SettingScreen from './android/app/src/screens/SettingScreen';
+import HomeScreen from './android/app/src/screens/home/HomeScreen';
+import ChatbotScreen from './android/app/src/screens/chatbot/ChatbotScreen';
+import SettingScreen from './android/app/src/screens/setting/SettingScreen';
 import BLEConnection from './android/app/src/screens/bluetooth/BLEConnection';
 import LoginScreen from './android/app/src/screens/auth/Login/LoginScreen';
 import IdFind from './android/app/src/screens/auth/Login/IdFInd';
@@ -14,10 +14,13 @@ import PasswordFind from './android/app/src/screens/auth/Login/PasswordFind';
 import RegisterEmail from './android/app/src/screens/auth/Register/RegisteEmail';
 import RegisterName from './android/app/src/screens/auth/Register/RegisterName';
 import RegisterPassword from './android/app/src/screens/auth/Register/RegisterPassword';
+import ChangePassword from './android/app/src/screens/setting/ChangePassword';
 import ConfirmPassword from './android/app/src/screens/auth/Register/CofirmPassword';
 import PersonalInfo from './android/app/src/screens/auth/Register/PersonalInfo';
 // React Query Client 생성
 // const queryClient = new QueryClient();s
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
 
 const Stack = createNativeStackNavigator();
 
@@ -30,7 +33,8 @@ const App = () => {
           <Stack.Screen name="Login" component={LoginScreen}/>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Chatbot" component={ChatbotScreen} />
-          <Stack.Screen name="Setting" component={SettingScreen} />
+          <Stack.Screen name="Setting" component={ChangePassword} />
+          <Stack.Screen name="ChangePassword" component={ChangePassword} />
           <Stack.Screen name="Info" component={PersonalInfo} />
           <Stack.Screen name="Email" component={RegisterEmail} />
           <Stack.Screen name="Name" component={RegisterName} />
